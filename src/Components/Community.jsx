ee49979b7115c7f0cdbfa8c4ed82a3bc9427b384
siva <sivamani_k@srmap.edu.in>
2024-06-19 10:10:58 +0530

    update

--- src/Components/Community.jsx
+++ src/Components/Community.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Community_nav from './Community_nav'
 import Community_hero from './Community_hero' 
+import Why_community from './Why_community'
 const Community = () => {
   return (
     <div>
@@ -9,7 +10,9 @@ const Community = () => {
         <Navbar />
         <Community_nav />
         <Community_hero />
+       
       </div>
+      <Why_community/>
     </div>
   )
 }
