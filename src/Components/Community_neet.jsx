e8403cf8ce2143c0d3f515fd8679a28b9aad0930
siva <sivamani_k@srmap.edu.in>
2024-06-20 11:00:25 +0530

    update

--- src/Components/Community_neet.jsx
+++ src/Components/Community_neet.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import Community_nav from './Community_nav'
 import Navbar from './Navbar'
 import Community_neet_hero from './Community_neet_hero'
-
+import Why_community from './Why_community'
+import Connect from './Connect'
+import Neet_members from './Neet_members'
+import Neet_conatct from './Neet_conatct'
+import Community_footer from './Community_footer'
 const Community_neet = () => {
   return (
    <div>
@@ -11,6 +15,11 @@ const Community_neet = () => {
      <Community_nav/>
     </div>
     <Community_neet_hero/>
+    <Why_community/>
+    <Connect />
+    <Neet_members/>
+    <Neet_conatct/>
+    <Community_footer/>
    </div>
   )
 }
--- src/Components/Connect.jsx
+++ src/Components/Connect.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import img1 from '../assets/child1.png';
-import img2 from '../assets/child2.png';
+import React,{useState} from 'react';
+import img1 from '../assets/neet1.png';
+import img2 from '../assets/neet2.png';
 import './Connect.css'; // Import your CSS file for styling
 
 const Connect = () => {
   return (
-    <div className="outerexpert bg-white">
+    <div className="outerexpert bg-[#F6FAFF]">
       <div className="innerexpert">
         <div className="content">
-          <h3 className='gradientheading'>Unlock Your Potential With Expert Guidance!</h3>
-          <p className='para'>Tailored advice and support from experienced mentors. Join Our Mentorship Program Today! Transform your aspirations into achievements with our dedicated mentors.</p>
+          <h3 className='gradientheading text-xl'>Connect and collabrate</h3>
+          
+          <ul className='list-disc mt-4 pl-5'>
+            <p className='text-xl mb-4 leading-8'>
+            Join a vibrant community of Writo who share your passion and drive.
+          </p>
+          <p className='text-xl mb-4 leading-8'>
+            Connect with individuals who are as excited about NEET Exam as you are, fostering an environment of collaboration and mutual growth.
+          </p>
+          <ul className='list-disc mt-4 pl-5'>
+            <li className='text-xl mt-6 sm:mt-2'>
+              Meet like minded people
+            </li>
+            <li className='text-xl mt-6 sm:mt-2'>
+              Participate in challenges, competitions, and win digital badges.
+            </li>
+            <li className='text-xl mt-6 sm:mt-2'>
+              Join the self-learning environment and explore on your own.
+            </li>
+          </ul>
+          </ul>
         </div>
 
         <div className="image-container">
