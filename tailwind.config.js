ebf29f56e998ed4214cac23b33fdb3523835cc7b
siva <sivamani_k@srmap.edu.in>
2024-06-28 10:37:40 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'height': 'max-height',
+      },
+      transitionDuration: {
+        
+        '400': '1000ms', // Even longer duration
+      },
+      maxHeight: {
+        '0': '0',
+        'screen': '100vh',
+      },
+      rotate:{
+        '27':'27deg',
+      },
+      insert:{
+        '17px':'17px'
+      },
+      skew:{
+        '35':'35deg'
+      }
+    },
   },
   plugins: [],
 }
