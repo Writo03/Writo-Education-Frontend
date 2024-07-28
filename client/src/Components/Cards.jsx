5a57b016526bbdfc3d3ebf5b4a7cbec3abad8202
5AIPAVAN <123063767+5AIPAVAN@users.noreply.github.com>
2024-07-28 20:20:24 +0530

    card color Update Cards.jsx
    card color Update Cards.jsx

--- client/src/Components/Cards.jsx
+++ client/src/Components/Cards.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 const Card = ({ title, description, icon }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-      <div className="flex items-center justify-center h-32 bg-green-500 text-white">
+      <div className="flex items-center justify-center h-32 bg-[#41c396] text-white">
         {icon}
       </div>
       <div className="p-4">
-        <h3 className="text-xl font-semibold text-green-600 mb-2">{title}</h3>
+        <h3 className="text-xl font-semibold text-[#41c396] mb-2">{title}</h3>
         <p className="text-gray-600">{description}</p>
       </div>
     </div>
