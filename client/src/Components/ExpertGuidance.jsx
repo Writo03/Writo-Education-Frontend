435d6597eb9bc96893ef42730c3088dd23ec9ca1
5AIPAVAN <123063767+5AIPAVAN@users.noreply.github.com>
2024-07-28 21:28:46 +0530

    Update ExpertGuidance.jsx
    Update ExpertGuidance.jsx

--- client/src/Components/ExpertGuidance.jsx
+++ client/src/Components/ExpertGuidance.jsx
@@ -16,7 +16,7 @@ export default function ExpertGuidance() {
 
             <p className='para'>Tailored advice and support from experienced mentors.Join Our Mentorship Program Today! Transform your aspirations into achievements with our dedicated mentors.</p>
             <Link to ='/mentorship'>
-            <button className="flex mr-auto text-white bg-[#488B80] border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded item3 font-semibold mt-20">Try our mentorship program</button>
+            <button className="flex mr-auto text-white bg-[#488B80] border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded item3 font-semibold ">Try our mentorship program</button>
             </Link>
         </div>
 
