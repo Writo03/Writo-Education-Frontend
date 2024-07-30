6b8dd8cf33c9f4fca9faa6a1bd6d7cb6034bfe88
siva <sivamani_k@srmap.edu.in>
2024-07-30 08:43:22 +0530

    update

--- client/src/Chatpages/Chathome.jsx
+++ client/src/Chatpages/Chathome.jsx
@@ -1,92 +1,91 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
-import { logout, setOnlineUser, setSocketConnection, setUser } from '../redux/userSlice'
-import Sidebar from '../Chatcomponents/Sidebar'
-import logo from '../assets/writo_logo.png'
-import io from 'socket.io-client'
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { logout, setOnlineUser, setSocketConnection, setUser } from '../redux/userSlice';
+import Sidebar from '../Chatcomponents/Sidebar';
+import io from 'socket.io-client';
 
 const Chathome = () => {
-  const user = useSelector(state => state.user)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const location = useLocation()
+  const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  console.log('user',user)
-  const fetchUserDetails = async()=>{
+  const fetchUserDetails = async () => {
     try {
-        const URL = `https://chat-app-main-2-copy.onrender.com/api/user-details`
-        const response = await axios({
-          url : URL,
-          withCredentials : true
-        })
+      const URL = `https://chat-app-main-2-copy.onrender.com/api/user-details`;
+      const response = await axios({
+        url: URL,
+        withCredentials: true
+      });
 
-        dispatch(setUser(response.data.data))
+      console.log("Backend Response:", response.data); // Log the response data
 
-        if(response.data.data.logout){
-            dispatch(logout())
-            navigate("/email")
-        }
-        console.log("current user Details", response);
-      } catch (error) {
-        console.error("Error fetching user details:", error);
-        if (error.response) {
-          console.error("Error Response:", error.response.data);
-        } else if (error.request) {
-          console.error("Error Request:", error.request);
-        } else {
-          console.error("General Error:", error.message);
-        }
-    } 
-  }
+      // Assuming response.data is the actual user data
+      dispatch(setUser(response.data.data));
 
-  useEffect(()=>{
-    fetchUserDetails()
-  },[])
+      if (response.data.data.logout) {
+        dispatch(logout());
+        navigate("/email");
+      }
 
-  /***socket connection */
-  useEffect(()=>{
-    const socketConnection = io('https://chat-app-main-2-copy.onrender.com',{
-      auth : {
-        token : localStorage.getItem('token')
-      },
-    })
-
-    socketConnection.on('onlineUser',(data)=>{
-      console.log(data)
-      dispatch(setOnlineUser(data))
-    })
-
-    dispatch(setSocketConnection(socketConnection))
-
-    return ()=>{
-      socketConnection.disconnect()
+      console.log("Current user details", response);
+    } catch (error) {
+      console.error("Error fetching user details:", error);
+      if (error.response) {
+        console.error("Error Response:", error.response.data);
+      } else if (error.request) {
+        console.error("Error Request:", error.request);
+      } else {
+        console.error("General Error:", error.message);
+      }
     }
-  },[])
+  };
 
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
 
-  const basePath = location.pathname === '/chat-home'
+  useEffect(() => {
+    const socketConnection = io('https://chat-app-main-2-copy.onrender.com', {
+      auth: {
+        token: localStorage.getItem('token')
+      },
+    });
+
+    socketConnection.on('onlineUser', (data) => {
+      console.log(data);
+      dispatch(setOnlineUser(data));
+    });
+
+    dispatch(setSocketConnection(socketConnection));
+
+    return () => {
+      socketConnection.disconnect();
+    };
+  }, []);
+
+  const basePath = location.pathname === '/chat-home';
   return (
     <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
-           <Sidebar/>
-        </section>
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+        <Sidebar />
+      </section>
 
-        {/**message component**/}
-        <section className={`${basePath && "hidden"}`} >
-            <Outlet/>
-        </section>
+      {/**message component**/}
+      <section className={`${basePath && "hidden"}`}>
+        <Outlet />
+      </section>
 
-
-        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
-            <div>
-              <h1 style={{fontSize:'30px',color:'#006756',fontWeight:'500'}}> Welcome to Writo Chat, Clear Your Doubts With Our Experts</h1>
-            </div>
-            <p className='text-lg mt-2 text-slate-500'>Select Mentors To Ask Your Doubts</p>
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}`}>
+        <div>
+          <h1 style={{ fontSize: '30px', color: '#006756', fontWeight: '500' }}> Welcome to Writo Chat, Clear Your Doubts With Our Experts</h1>
         </div>
+        <p className='text-lg mt-2 text-slate-500'>Select Mentors To Ask Your Doubts</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chathome
+export default Chathome;
--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -17,6 +17,7 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     setUser : (state,action)=>{
+      console.log("Action Payload:", action.payload);
         state._id = action.payload._id
         state.name = action.payload.name 
         state.email = action.payload.email 
