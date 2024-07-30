2a9fd471349bb40cefb7db077f0a8daa973342e7
siva <sivamani_k@srmap.edu.in>
2024-07-30 20:19:35 +0530

    update

--- server/controller/userDetails.js
+++ server/controller/userDetails.js
@@ -1,31 +1,21 @@
-const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
+const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken")
 
-async function userDetails(request, response) {
+async function userDetails(request,response){
     try {
-        const token = request.cookies.token;
-        console.log('Token:', token);
+        const token = request.cookies.token || ""
 
-        const user = await getUserDetailsFromToken(token);
-        console.log('User:', user);
-
-        if (user.logout) {
-            return response.status(401).json({
-                message: "Session expired",
-                logout: true,
-            });
-        }
+        const user = await getUserDetailsFromToken(token)
 
         return response.status(200).json({
-            message: "User details",
-            data: user,
-        });
+            message : "user details",
+            data : user
+        })
     } catch (error) {
-        console.error("Error:", error);
         return response.status(500).json({
-            message: error.message || error,
-            error: true,
-        });
+            message : error.message || error,
+            error : true
+        })
     }
 }
 
-module.exports = userDetails;
+module.exports = userDetails
--- server/helpers/getUserDetailsFromToken.js
+++ server/helpers/getUserDetailsFromToken.js
@@ -1,25 +1,20 @@
-const jwt = require('jsonwebtoken');
-const UserModel = require('../models/UserModel');
+const jwt = require('jsonwebtoken')
+const UserModel = require('../models/UserModel')
 
-const getUserDetailsFromToken = async (token) => {
+const getUserDetailsFromToken = async(token)=>{
     
-    if (!token) {
+    if(!token){
         return {
-            message: "session out",
-            logout: true,
-        };
+            message : "session out",
+            logout : true,
+        }
     }
 
-    try {
-        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        const user = await UserModel.findById(decode.id).select('-password');
-        return user;
-    } catch (error) {
-        return {
-            message: "session out",
-            logout: true,
-        };
-    }
-};
+    const decode = await jwt.verify(token,process.env.JWT_SECREAT_KEY)
 
-module.exports = getUserDetailsFromToken;
+    const user = await UserModel.findById(decode.id).select('-password')
+
+    return user
+}
+
+module.exports = getUserDetailsFromToken
