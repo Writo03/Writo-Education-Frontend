28810ffe2111330138ee13900f006110c07724f9
Writo03 <88578386+Writo03@users.noreply.github.com>
2024-08-04 01:12:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const session = require('express-session')
 app.use(cors({
     origin :"*",
     methods:["GET","POST","PUT","DELETE"],
-    credentials : false
+    credentials : true
 }))
 app.use(express.json())
 app.use(cookiesParser())
