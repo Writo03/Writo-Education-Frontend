e5e88b5e4a920cad4efea61c6d2bc713bafe0451
Writo03 <88578386+Writo03@users.noreply.github.com>
2024-08-04 00:28:44 +0530

    Update index.js
    update

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const session = require('express-session')
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "React app URL"
+    "https://writo-education-frontend.onrender.com/api/payment/orders"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
