706d76d9bccb2bad8dc1642ae6b69bc92db91f86
siva <sivamani_k@srmap.edu.in>
2024-08-04 19:34:46 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,64 +1,62 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const connectDB = require('./config/connectDB')
-const router = require('./routes/index')
-const mentorRoutes = require('./routes/mentorRoutes')
-const cookiesParser = require('cookie-parser')
-const { app, server } = require('./socket/index')
-const paymentRoutes = require('./routes/payment')
-const session = require('express-session')
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/connectDB');
+const router = require('./routes/index');
+const mentorRoutes = require('./routes/mentorRoutes');
+const cookieParser = require('cookie-parser');
+const { app, server } = require('./socket/index');
+const paymentRoutes = require('./routes/payment');
+const session = require('express-session');
 
 // const app = express()
 app.use(cors({
-    origin :"*",
-    methods:["GET","POST","PUT","DELETE"],
-    credentials : true
-}))
-app.use(express.json())
-app.use(cookiesParser())
+    origin: 'https://writo-education-frontend.vercel.app', // Replace '*' with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(session({
-    secret:'secret',
-    resave:false,
-    saveUninitialized:false,
-    cookie:{
-        secure:true,
-        maxAge:1000*60*60*24
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24
     }
-}))
+}));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
     response.json({
-        message : "Server running at " + PORT
-    })
-})
+        message: "Server running at " + PORT
+    });
+});
 
-app.get('/id',(req,res)=>{
-    if(req.session.Id){
-        res.json({valid:true,id:req.session.Id})
+app.get('/id', (req, res) => {
+    if (req.session.Id) {
+        res.json({ valid: true, id: req.session.Id });
+    } else {
+        res.json({ valid: false });
     }
-    else{
-        res.json({valid:false})
-    }
-})
+});
 
-app.get('/remove-id',(req,res)=>{
-    console.log(req.session.Id)
-    req.session.Id=null
-    res.json({message:req.session.Id})
-    
-})
+app.get('/remove-id', (req, res) => {
+    console.log(req.session.Id);
+    req.session.Id = null;
+    res.json({ message: req.session.Id });
+});
 
-//api endpoints
-app.use('/api',router)
+// api endpoints
+app.use('/api', router);
 app.use('/api/mentors', mentorRoutes);
-app.use("/api/payment/", paymentRoutes);
+app.use('/api/payment/', paymentRoutes);
 
-connectDB().then(()=>{
-    server.listen(PORT,()=>{
-        console.log("server running at " + PORT)
-    })
-})
+connectDB().then(() => {
+    server.listen(PORT, () => {
+        console.log("server running at " + PORT);
+    });
+});
