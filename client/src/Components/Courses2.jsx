ffbeffac7bd3c80fc6b177b7bb8c3bc713587c2c
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-10-03 21:31:20 +0530

    responsiveness in the home page

--- client/src/Components/Courses2.jsx
+++ client/src/Components/Courses2.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import './Courses2.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Courses2.css";
+import { Link } from "react-router-dom";
 
 export default function Courses2() {
-    return (
-        <div className="outercourse2 my-1">
-            <div className="innercourse2">
-
-            <Link to ='/class-11'>
-            <div className="outertype2">
-                <div className="type2">
-                    <h4> Class 11</h4>
-                    <h5> JEE/NEET Exam</h5>
-                </div>
-                </div>
-                </Link>
-
-                <Link to ='/class-12'>
-                <div className="outertype2">
-                <div className="type2">
-                    <h4> Class 12</h4>
-                    <h5> JEE/NEET Exam</h5>
-                </div>
-                </div>
-                </Link>
-         
+  return (
+    <div className="outercourse2 my-1">
+      <div className="innercourse2 text-xs md:text-sm lg:text-base">
+        <Link to="/class-11">
+          <div className="outertype2">
+            <div className="type2">
+              <h4> Class 11</h4>
+              <h5> JEE/NEET Exam</h5>
             </div>
-        </div>
-    )
+          </div>
+        </Link>
+        <Link to="/class-12">
+          <div className="outertype2">
+            <div className="type2">
+              <h4> Class 12</h4>
+              <h5> JEE/NEET Exam</h5>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- client/src/Components/HeroSection.jsx
+++ client/src/Components/HeroSection.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import HeroImage from '../assets/hero-image.png';
+import React from "react";
+import HeroImage from "../assets/hero-image.png";
 
 const HeroSection = () => {
   return (
-    <div className="relative z-10 hero-section custom-gradient "> {/* Ensure z-index is lower than Services */}
+    <div className="relative z-10 hero-section custom-gradient ">
+      {/* Ensure z-index is lower than Services */}
       <section className="hero ">
         <div className="hero-text item1">
-          <h1 className='text-green-900'><span>Thrive with our tailored mentorship</span></h1>
-          <p className='text-3xl'>Unlock your potential with our interactive online classes, where learning knows no boundaries.</p>
-          <button>Book your individual classes now</button>
+          <h1 className="text-green-900">
+            <span>Thrive with our tailored mentorship</span>
+          </h1>
+          <p className="text-3xl">
+            Unlock your potential with our interactive online classes, where
+            learning knows no boundaries.
+          </p>
+          <button /* className="border border-[#008000]" */>
+            <span className="underline decoration-[#ffa726] text-xl">
+              Book your individual classes now
+            </span>
+          </button>
         </div>
         <div className="hero-image flex justify-center items-center relative">
           <div className="image-wrapper p-2 rounded-full border-2 border-dashed border-green-800">
-            <img 
-              src={HeroImage} 
-              alt="Students studying" 
-              className="rounded-full" 
+            <img
+              src={HeroImage}
+              alt="Students studying"
+              className="rounded-full"
             />
           </div>
           <div className="student-count absolute bottom-0 left-1/6 transform -translate-x-1/2 translate-y-1/2 bg-white px-8 py-2 rounded shadow">
@@ -24,31 +34,30 @@ const HeroSection = () => {
           </div>
         </div>
       </section>
-
       {/* Add some space below the hero section */}
-      <div className="my-20"></div>
-
-      <section className="stats-box w-11/12 md:w-1/2 mx-auto rounded-lg  p-4 border border-gray-300 shadow-lg bg-[rgba(26,183,157,0.74)]">
-        
-        <div className="flex flex-wrap justify-around text-center space-y-2 md:space-y-0 md:space-x-0 divide-x divide-gray-300">
-          <div className="mb-2 w-1/2 md:w-auto px-2 md:px-4 flex-1">
-            <h3 className="text-lg md:text-xl font-bold">12000+/</h3>
-            <p className="text-gray-700 text-sm md:text-base">Students</p>
+      {/* <div className="my-20"></div> */}
+      <div className="py-6">
+        <section className="stats-box w-11/12 md:w-1/2 mx-auto rounded-lg  p-4 border border-gray-300 shadow-lg bg-[rgba(26,183,157,0.74)]">
+          <div className="flex flex-wrap justify-around text-center space-y-2 md:space-y-0 md:space-x-0 divide-x divide-gray-300">
+            <div className="mb-2 w-1/2 md:w-auto px-2 md:px-4 flex-1">
+              <h3 className="text-lg md:text-xl font-bold">12000+/</h3>
+              <p className="text-gray-700 text-sm md:text-base">Students</p>
+            </div>
+            <div className="mb-2 w-1/2 md:w-auto px-2 md:px-4 flex-1">
+              <h3 className="text-lg md:text-xl font-bold">30+/</h3>
+              <p className="text-gray-700 text-sm md:text-base">Classes</p>
+            </div>
+            <div className="mb-2 w-1/2 md:w-auto px-2 md:px-4 flex-1">
+              <h3 className="text-lg md:text-xl font-bold">100+/</h3>
+              <p className="text-gray-700 text-sm md:text-base">Mentors</p>
+            </div>
+            <div className="mb-2 w-1/2 md:w-auto px-2 md:px-4 flex-1">
+              <h3 className="text-lg md:text-xl font-bold">50+/</h3>
+              <p className="text-gray-700 text-sm md:text-base">Subjects</p>
+            </div>
           </div>
-          <div className="mb-2 w-1/2 md:w-auto px-2 md:px-4 flex-1">
-            <h3 className="text-lg md:text-xl font-bold">30+/</h3>
-            <p className="text-gray-700 text-sm md:text-base">Classes</p>
-          </div>
-          <div className="mb-2 w-1/2 md:w-auto px-2 md:px-4 flex-1">
-            <h3 className="text-lg md:text-xl font-bold">100+/</h3>
-            <p className="text-gray-700 text-sm md:text-base">Mentors</p>
-          </div>
-          <div className="mb-2 w-1/2 md:w-auto px-2 md:px-4 flex-1">
-            <h3 className="text-lg md:text-xl font-bold">50+/</h3>
-            <p className="text-gray-700 text-sm md:text-base">Subjects</p>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
