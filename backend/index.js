2fc1505b22b0f00d7cc641b44cb0c713ad4e917c
siva <sivamani_k@srmap.edu.in>
2024-10-06 09:01:31 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const { app, server } = require('./socket/index')
 const session = require('express-session')
 
 // const app = express()
-const allowedOrigins = ['http://localhost:3000', 'https://writo-education-frontend.onrender.com'];
+const allowedOrigins = ['http://localhost:3000', 'https://writo-education-frontend.onrender.com',"https://writo-education-frontend.onrender.com/api/login"];
 app.use(cors({
   origin: allowedOrigins,
   credentials: true, 
