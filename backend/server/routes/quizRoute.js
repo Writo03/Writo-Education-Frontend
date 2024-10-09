9b385fbc5655e420c9e2877a85c896578bfa908f
siva <sivamani_k@srmap.edu.in>
2024-10-09 17:07:08 +0530

    update

--- backend/server/routes/quizRoute.js
+++ backend/server/routes/quizRoute.js
@@ -4,4 +4,12 @@ const quizController = require('../controller/quizcontroller')
 
 router.post('/add-quiz',quizController.add_quiz)
 
+router.get('/get-quizes',quizController.get_quizes)
+
+router.get('/get-quiz/:id',quizController.get_quiz)
+
+router.put('/update-quiz/:id',quizController.update_quiz)
+
+router.delete('/delete-quiz/:id',quizController.delete_quiz)
+
 module.exports = router
