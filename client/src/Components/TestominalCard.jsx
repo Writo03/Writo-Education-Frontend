44d47df4d60f86b578e65919ed8350b65160f3ad
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-10-11 11:03:13 +0530

    home testimonial parent working

--- client/src/Components/TestominalCard.jsx
+++ client/src/Components/TestominalCard.jsx
@@ -1,37 +1,97 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { IoMdStar, IoMdStarOutline } from "react-icons/io";
 import { FaCircle } from "react-icons/fa";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from "react-responsive";
 
-const TestimonialCard = ({ testimonials }) => {
-  const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' });
+const TestimonialCard = ({ stestimonials, ptestimonials }) => {
+  const [isstudent, setIsstudent] = useState("student");
+  return (
+    <section className="body-font text-gray-600">
+      <div className="container mx-auto px-5 py-6">
+        <div className="flex items-center justify-center">
+          <p className="item5 text-center text-2xl text-[#ED7E0A]">
+            - - - - Our love - - -
+          </p>
+        </div>
+        <div className="mb-4 mt-6 flex items-center justify-center">
+          <p className="item1 mt-2 text-center text-4xl text-[#488B80]">
+            Testimonials
+          </p>
+        </div>
+        <div className="mt-4 flex items-center justify-center">
+          <button
+            className={`item2 mr-4 rounded-br-2xl rounded-tl-2xl px-4 py-2 text-2xl hover:text-black ${isstudent === "student" ? "bg-[#54C8B4]" : "bg-white text-black"}`}
+            onClick={() => setIsstudent("student")}
+          >
+            Students
+          </button>
+          <button
+            className={`item2 ml-4 rounded-br-2xl rounded-tl-2xl px-6 py-2 text-2xl hover:text-black ${isstudent === "parent" ? "bg-[#54C8B4]" : "bg-white text-black"}`}
+            onClick={() => setIsstudent("parent")}
+          >
+            Parents
+          </button>
+        </div>
+        {isstudent === "student" ? (
+          <TestimonialCardHelper testimonials={stestimonials} />
+        ) : (
+          <TestimonialCardHelper testimonials={ptestimonials} />
+        )}
+      </div>
+    </section>
+  );
+};
+
+export function TestimonialCardHelper({ testimonials }) {
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
   const testimonialsPerPage = isSmallScreen ? 1 : 3;
   const [startIndex, setStartIndex] = useState(0);
   const [activeDot, setActiveDot] = useState(0);
   const [sliderInterval, setSliderInterval] = useState(null);
 
+  const visibleTestimonials = testimonials.slice(
+    startIndex,
+    startIndex + testimonialsPerPage,
+  );
+
   const handlePrev = () => {
     clearInterval(sliderInterval);
-    setStartIndex((prevIndex) => prevIndex === 0 ? testimonials.length - testimonialsPerPage : prevIndex - 1);
-    setActiveDot((prevDot) => prevDot === 0 ? testimonialsPerPage - 1 : prevDot - 1);
+    setStartIndex((prevIndex) =>
+      prevIndex === 0
+        ? testimonials.length - testimonialsPerPage
+        : prevIndex - 1,
+    );
+    setActiveDot((prevDot) =>
+      prevDot === 0 ? testimonialsPerPage - 1 : prevDot - 1,
+    );
     startSlider();
   };
 
   const handleNext = () => {
     clearInterval(sliderInterval);
-    setStartIndex((prevIndex) => prevIndex === testimonials.length - testimonialsPerPage ? 0 : Math.min(testimonials.length - testimonialsPerPage, prevIndex + 1));
-    setActiveDot((prevDot) => prevDot === testimonialsPerPage - 1 ? 0 : prevDot + 1);
+    setStartIndex((prevIndex) =>
+      prevIndex === testimonials.length - testimonialsPerPage
+        ? 0
+        : Math.min(testimonials.length - testimonialsPerPage, prevIndex + 1),
+    );
+    setActiveDot((prevDot) =>
+      prevDot === testimonialsPerPage - 1 ? 0 : prevDot + 1,
+    );
     startSlider();
   };
-
-  const visibleTestimonials = testimonials.slice(startIndex, startIndex + testimonialsPerPage);
 
   const startSlider = () => {
     clearInterval(sliderInterval);
     const interval = setInterval(() => {
-      setStartIndex((prevIndex) => prevIndex === testimonials.length - testimonialsPerPage ? 0 : Math.min(testimonials.length - testimonialsPerPage, prevIndex + 1));
-      setActiveDot((prevDot) => prevDot === testimonialsPerPage - 1 ? 0 : prevDot + 1);
+      setStartIndex((prevIndex) =>
+        prevIndex === testimonials.length - testimonialsPerPage
+          ? 0
+          : Math.min(testimonials.length - testimonialsPerPage, prevIndex + 1),
+      );
+      setActiveDot((prevDot) =>
+        prevDot === testimonialsPerPage - 1 ? 0 : prevDot + 1,
+      );
     }, 10000);
     setSliderInterval(interval);
   };
@@ -42,62 +102,59 @@ const TestimonialCard = ({ testimonials }) => {
   }, [isSmallScreen]);
 
   return (
-    <section className="text-gray-600 body-font">
-      <div className="container px-5 py-6 mx-auto">
-        <div className="flex justify-center items-center">
-          <p className='text-center text-[#ED7E0A] text-2xl item5'>- - - -   Our love  - - - </p>
-        </div>
-        <div className="flex justify-center items-center mb-4 mt-6">
-          <p className='text-center text-[#488B80] text-4xl mt-2 item1'>Testimonials</p>
-        </div>
-        <div className="flex justify-center items-center mt-4">
-          <button className="mr-4 text-2xl bg-[#54C8B4] text-white hover:bg-white item2 hover:text-[#54C8B4] py-2 px-4 rounded-tl-2xl rounded-br-2xl">
-            Students
-          </button>
-          <button className="ml-4 text-2xl bg-[#54C8B4] text-white hover:bg-white item2 hover:text-[#54C8B4] py-2 px-6 rounded-tl-2xl rounded-br-2xl">
-            Parents
-          </button>
-        </div>
-        <div className="flex flex-wrap -m-4 mt-4">
-          {visibleTestimonials.map((testimonial, index) => (
-            <div key={index} className="lg:w-1/3 lg:mb-0 mb-6 p-4">
-              <div className="h-full text-center">
-                <img
-                  alt="testimonial"
-                  className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                  src={testimonial.image}
-                />
-                <h2 className="text-gray-900 title-font tracking-wider font-semibold text-sm item4">
-                  {testimonial.name}
-                </h2>
-                <div className="flex justify-center mt-2">
-                  {[...Array(testimonial.stars)].map((_, index) => (
-                    <IoMdStar key={index} className="text-[#FFE249] text-2xl" />
-                  ))}
-                  {[...Array(5 - testimonial.stars)].map((_, index) => (
-                    <IoMdStarOutline key={index} className="text-[#E4E4E4] text-2xl" />
-                  ))}
-                </div>
-                <p className="leading-relaxed">{testimonial.text}</p>
-                <span className="inline-block h-1 w-10 rounded bg-[#54C8B4] mt-6 mb-4"></span>
+    <>
+      <div className="-m-4 mt-4 flex flex-wrap">
+        {visibleTestimonials.map((testimonial, index) => (
+          <div key={index} className="mb-6 p-4 lg:mb-0 lg:w-1/3">
+            <div className="h-full text-center">
+              <img
+                alt="testimonial"
+                className="mb-8 inline-block h-20 w-20 rounded-full border-2 border-gray-200 bg-gray-100 object-cover object-center"
+                src={testimonial.image}
+              />
+              <h2 className="title-font item4 text-sm font-semibold tracking-wider text-gray-900">
+                {testimonial.name}
+              </h2>
+              <div className="mt-2 flex justify-center">
+                {[...Array(testimonial.stars)].map((_, index) => (
+                  <IoMdStar key={index} className="text-2xl text-[#FFE249]" />
+                ))}
+                {[...Array(5 - testimonial.stars)].map((_, index) => (
+                  <IoMdStarOutline
+                    key={index}
+                    className="text-2xl text-[#E4E4E4]"
+                  />
+                ))}
               </div>
+              <p className="leading-relaxed">{testimonial.text}</p>
+              <span className="mb-4 mt-6 inline-block h-1 w-10 rounded bg-[#54C8B4]"></span>
             </div>
-          ))}
-        </div>
-        <div className="flex justify-center items-center mt-4">
-          <button onClick={handlePrev} className="mr-4 text-2xl text-gray-800 hover:text-[#54C8B4] font-semibold py-2 px-4 rounded-l">
-            <FaArrowLeft />
-          </button>
-          {[...Array(testimonialsPerPage)].map((_, index) => (
-            <FaCircle key={index} className={`text-xs ${index === activeDot ? 'text-[#54C8B4] mr-4' : 'text-[#D9D9D9]'} mx-2 cursor-pointer`} onClick={() => setStartIndex(index * testimonialsPerPage)} />
-          ))}
-          <button onClick={handleNext} className="ml-4 text-2xl hover:text-[#54C8B4] text-gray-800 font-semibold py-2 px-4 rounded-r">
-            <FaArrowRight />
-          </button>
-        </div>
+          </div>
+        ))}
       </div>
-    </section>
+      <div className="mt-4 flex items-center justify-center">
+        <button
+          onClick={handlePrev}
+          className="mr-4 rounded-l px-4 py-2 text-2xl font-semibold text-gray-800 hover:text-[#54C8B4]"
+        >
+          <FaArrowLeft />
+        </button>
+        {[...Array(testimonialsPerPage)].map((_, index) => (
+          <FaCircle
+            key={index}
+            className={`text-xs ${index === activeDot ? "mr-4 text-[#54C8B4]" : "text-[#D9D9D9]"} mx-2 cursor-pointer`}
+            onClick={() => setStartIndex(index * testimonialsPerPage)}
+          />
+        ))}
+        <button
+          onClick={handleNext}
+          className="ml-4 rounded-r px-4 py-2 text-2xl font-semibold text-gray-800 hover:text-[#54C8B4]"
+        >
+          <FaArrowRight />
+        </button>
+      </div>
+    </>
   );
-};
+}
 
 export default TestimonialCard;
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -5,7 +5,8 @@ import HeroSection from "./Components/HeroSection";
 
 import About from "./Components/About";
 import TestimonialCard from "./Components/TestominalCard";
-import image from "./assets/image copy.png";
+import simage from "./assets/image copy.png";
+import pimage from "./assets/dp_img.png";
 
 import Contactus from "./Components/Contactus";
 import Test from "./Components/Test";
@@ -19,35 +20,54 @@ import Courses2 from "./Components/Courses2";
 import Navbar2 from "./Components/Navbar2";
 const Home = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const testimonialsData = [
+  const stestimonialsData = [
     {
       name: "Henry Letham",
       text: "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
-      image: image,
+      image: simage,
       stars: 4,
     },
     {
       name: "Holden Caulfield",
       text: "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
-      image: image,
+      image: simage,
       stars: 3,
     },
     {
       name: "Alper Kamu",
       text: "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
-      image: image,
+      image: simage,
       stars: 5,
     },
     {
       name: "Henry Letham",
       text: "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
-      image: image,
+      image: simage,
       stars: 2,
+    },
+  ];
+  const ptestimonialsData = [
+    {
+      name: "Henry Letham",
+      text: "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      image: pimage,
+      stars: 4,
+    },
+    {
+      name: "Holden Caulfield",
+      text: "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      image: pimage,
+      stars: 3,
+    },
+    {
+      name: "Alper Kamu",
+      text: "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      image: pimage,
+      stars: 5,
     },
   ];
   return (
     <div>
-      
       <Navbar2 />
       <div className="bg1 w-full overflow-hidden">
         <HeroSection dropdownOpen={dropdownOpen} />
@@ -60,7 +80,10 @@ const Home = () => {
       <ExpertGuidance />
       <Doubt />
       <Test />
-      <TestimonialCard testimonials={testimonialsData} />
+      <TestimonialCard
+        stestimonials={stestimonialsData}
+        ptestimonials={ptestimonialsData}
+      />
       {/* kaushik work left - pagination is not working */}
       <Demo />
       <Contactus />
