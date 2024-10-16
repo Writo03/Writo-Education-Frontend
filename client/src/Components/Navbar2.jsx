4e16ea295c8e4c49751752a993f7a27555466633
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-10-16 21:33:04 +0530

    home hero nav searchbox fix

--- client/src/Components/Navbar2.jsx
+++ client/src/Components/Navbar2.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Services from "./Services";
 import Events from "./Events";
 import "./NavBar2.css";
+import { CiSearch } from "react-icons/ci";
 
 const NavBar2 = () => {
   const [showServices, setShowServices] = useState(false);
@@ -48,14 +49,17 @@ const NavBar2 = () => {
           {/* <button className="px-4 py-1 border  border-gray-400  rounded-lg hover:border-black text-xs md:text-base">
             Search class
           </button> */}
-          <input
-            type="text"
-            name="search"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="w-20 rounded-lg border border-[#488B80] px-4 py-1 text-xs hover:border-black md:w-1/4 md:text-base"
-            placeholder="Search"
-          />
+          <div className="relative flex w-20 items-center justify-center text-xs md:w-1/2 md:text-base">
+            <CiSearch className="absolute left-2" />
+            <input
+              type="text"
+              name="search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="h-full w-full rounded-lg border border-[#488B80] px-4 py-3 pl-6 hover:border-black"
+              placeholder="Search"
+            />
+          </div>
           <button className="rounded bg-[#488B80] px-2 py-1 text-xs text-white hover:bg-[#e5e7eb] hover:text-black md:text-base">
             View plans
           </button>
