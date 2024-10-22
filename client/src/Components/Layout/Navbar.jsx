1d39e4d9db8ac435af55308ac35ac8460787fd57
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-10-22 22:07:59 +0530

    /home navbar improved

--- client/src/Components/Layout/Navbar.jsx
+++ client/src/Components/Layout/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { IoMenu, IoClose } from "react-icons/io5";
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import img from '../../assets/rito-removebg-preview.png';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import img from "../../assets/rito-removebg-preview.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,19 +9,18 @@ const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-
   // Check for the token in localStorage on component mount
   useEffect(() => {
-    console.log('hello')
+    console.log("hello");
     const checkAuth = () => {
-      const token = localStorage.getItem('token');
-      console.log('Token found:', token);  // Debugging log
+      const token = localStorage.getItem("token");
+      console.log("Token found:", token); // Debugging log
       if (token) {
         setAuth(true);
-        console.log('User is authenticated');
+        console.log("User is authenticated");
       } else {
         setAuth(false);
-        console.log('User is not authenticated');
+        console.log("User is not authenticated");
       }
     };
 
@@ -31,12 +30,11 @@ const Navbar = () => {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setAuth(false);
-    console.log('User logged out');
-    navigate('/')
+    console.log("User logged out");
+    navigate("/");
     setShowLogoutModal(false);
-
   };
 
   const handleCloseModal = () => {
@@ -44,150 +42,154 @@ const Navbar = () => {
   };
 
   const handleSignin = () => {
-    console.log('Navigating to Register');
-    navigate('/register', {
+    console.log("Navigating to Register");
+    navigate("/register", {
       state: {
         previousUrl: location.pathname,
-      }
+      },
     });
   };
 
   const handleLogin = () => {
-    console.log('Navigating to Login');
-    navigate('/email', {
+    console.log("Navigating to Login");
+    navigate("/email", {
       state: {
         previousUrl: location.pathname,
-      }
+      },
     });
   };
 
-  const handleTestSeries=()=>{
-    if(auth) navigate('/test_series')
-    else{
-      navigate('/register')
+  const handleTestSeries = () => {
+    if (auth) navigate("/test_series");
+    else {
+      navigate("/register");
     }
-  }
+  };
 
   return (
     <>
-      <header className={`text-gray-600 body-font relative w-full border-b-2`}>
-        <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row md:items-center justify-between">
+      <header className={`body-font relative w-full border-b-2 text-gray-600`}>
+        <div className="mx-auto flex flex-col flex-wrap justify-between p-5 md:flex-row md:items-center">
           {/* Logo and Title */}
-          <div className="flex items-center justify-between w-full md:w-auto">
-            <Link to="/" className="font-medium text-gray-900 flex items-center justify-center md:ml-6">
+          <div className="flex w-full items-center justify-between md:w-auto">
+            <Link
+              to="/"
+              className="flex items-center justify-center font-medium text-gray-900 md:ml-6"
+            >
               <img
                 src={img}
                 alt="Logo"
-                className="md:w-[8vw] md:h-[2vw] w-[12vw] h-[4vw]"
+                className="h-[4vw] w-[12vw] md:h-[2vw] md:w-[8vw]"
               />
             </Link>
             <button
-              className={`md:hidden h-8 ${isOpen ? 'bg-gray-200' : 'bg-[#488B80]'} border-0 py-1 px-4 focus:outline-none rounded text-base transition-all ease-in duration-100`}
+              className={`h-8 md:hidden ${isOpen ? "bg-gray-200" : "bg-[#488B80]"} rounded border-0 px-4 py-1 text-base transition-all duration-100 ease-in focus:outline-none`}
               onClick={() => setIsOpen(true)}
             >
-              <IoMenu className="text-white text-xl" />
+              <IoMenu className="text-xl text-white" />
             </button>
           </div>
 
           {/* Navigation Links */}
-          <div className='md:flex md:items-center md:w-auto w-full'>
-            
-          <nav
-            className={`md:flex md:items-center flex-col md:flex-row fixed md:static md:top-auto right-0 top-0 md:right-auto bg-[#f5f5f5] z-20 transition-all duration-300 ease-in-out w-2/3 md:w-auto h-screen md:h-auto overflow-y-auto md:overflow-visible pr-4 md:pr-0 pl-1 md:pl-0 ${
-              isOpen
-                ? "translate-x-0"
-                : "translate-x-full md:translate-x-0"
-            }`}
-          >
-            <div className='md:hidden'>
-            <IoClose onClick={() => setIsOpen(false)} className='text-2xl text-black mt-2 h-6 w-6 ml-1'/>
-            </div>
-            <button
-              onClick={handleTestSeries}
-              className="block text-start py-2 px-2 lg:px-2 text-lg lg:text-2xl text-black hover:text-[#488B80"
+          <div className="w-full md:flex md:w-auto md:items-center">
+            <nav
+              className={`fixed right-0 top-0 z-20 h-screen w-2/3 flex-col overflow-y-auto bg-[#f5f5f5] pl-1 pr-4 transition-all duration-300 ease-in-out md:static md:right-auto md:top-auto md:flex md:h-auto md:w-auto md:flex-row md:items-center md:overflow-visible md:pl-0 md:pr-0 ${
+                isOpen ? "translate-x-0" : "translate-x-full md:translate-x-0"
+              }`}
             >
-              Test Series
-            </button>
-            <Link
-              to="/mentorship"
-              className="block py-2 px-2 lg:px-2 text-lg lg:text-2xl text-black hover:text-[#488B80]"
-            >
-              Mentorship
-            </Link>
-            <Link
-              to="/community"
-              className="block py-2 px-2 lg:px-2 text-lg lg:text-2xl text-black hover:text-[#488B80]"
-            >
-              Community
-            </Link>
-            <Link
-              to="/careers"
-              className="block py-2 px-2 lg:px-2 text-lg lg:text-2xl text-black hover:text-[#488B80]"
-            >
-              Careers
-            </Link>
-            <Link
-              to="/blogs"
-              className="block py-2 px-2 lg:px-2 text-lg lg:text-2xl text-black hover:text-[#488B80]"
-            >
-              Blogs
-            </Link>
-            <Link
-              to="/sgp"
-              className="block py-2 px-2 lg:px-2 text-lg lg:text-2xl text-black hover:text-[#488B80]"
-            >
-              SGP
-            </Link>
-            {!auth && (
-              <div className="md:hidden flex flex-col mt-4">
-                <button
-                  onClick={handleSignin}
-                  className="bg-[#488B80] border-0 py-3 px-6 mb-2 focus:outline-none hover:bg-gray-200 rounded text-base hover:text-black text-white"
-                >
-                  Sign up
-                </button>
-                <button
-                  onClick={handleLogin}
-                  className="border border-[#488B80] py-3 px-6 focus:outline-none hover:bg-gray-200 rounded text-base"
-                >
-                  Login
-                </button>
+              <div className="md:hidden">
+                <IoClose
+                  onClick={() => setIsOpen(false)}
+                  className="ml-1 mt-2 h-6 w-6 text-2xl text-black"
+                />
               </div>
-            )}
-            {auth && (
-              <div className="md:hidden flex flex-col mt-4">
-                <button
-                  className="bg-[#488B80] border-0 py-3 px-6 focus:outline-none hover:bg-gray-200 rounded text-base hover:text-black text-white"
-                  onClick={() => setShowLogoutModal(true)}
-                >
-                  Logout
-                </button>
-              </div>
-            )}
-          </nav>
+              <button
+                onClick={handleTestSeries}
+                className="hover:text-[#488B80 block px-2 py-2 text-start text-lg text-black lg:px-2 lg:text-2xl"
+              >
+                Test Series
+              </button>
+              <Link
+                to="/mentorship"
+                className="block px-2 py-2 text-lg text-black hover:text-[#488B80] lg:px-2 lg:text-2xl"
+              >
+                Mentorship
+              </Link>
+              <Link
+                to="/community"
+                className="block px-2 py-2 text-lg text-black hover:text-[#488B80] lg:px-2 lg:text-2xl"
+              >
+                Community
+              </Link>
+              <Dropdown />
+              {/* <Link
+                to="/careers"
+                className="block px-2 py-2 text-lg text-black hover:text-[#488B80] lg:px-2 lg:text-2xl"
+              >
+                Careers
+              </Link>
+              <Link
+                to="/blogs"
+                className="block px-2 py-2 text-lg text-black hover:text-[#488B80] lg:px-2 lg:text-2xl"
+              >
+                Blogs
+              </Link>
+              <Link
+                to="/sgp"
+                className="block px-2 py-2 text-lg text-black hover:text-[#488B80] lg:px-2 lg:text-2xl"
+              >
+                SGP
+              </Link> */}
+              {!auth && (
+                <div className="mt-4 flex flex-col md:hidden">
+                  <button
+                    onClick={handleSignin}
+                    className="mb-2 rounded border-0 bg-[#488B80] px-6 py-3 text-base text-white hover:bg-gray-200 hover:text-black focus:outline-none"
+                  >
+                    Sign up
+                  </button>
+                  <button
+                    onClick={handleLogin}
+                    className="rounded border border-[#488B80] px-6 py-3 text-base hover:bg-gray-200 focus:outline-none"
+                  >
+                    Login
+                  </button>
+                </div>
+              )}
+              {auth && (
+                <div className="mt-4 flex flex-col md:hidden">
+                  <button
+                    className="rounded border-0 bg-[#488B80] px-6 py-3 text-base text-white hover:bg-gray-200 hover:text-black focus:outline-none"
+                    onClick={() => setShowLogoutModal(true)}
+                  >
+                    Logout
+                  </button>
+                </div>
+              )}
+            </nav>
           </div>
 
           {/* Buttons */}
           {!auth && (
-            <div className="hidden md:flex items-center">
+            <div className="hidden items-center md:flex">
               <button
                 onClick={handleSignin}
-                className="bg-[#488B80] border-0 py-2 lg:py-3 px-6 mr-3 focus:outline-none hover:bg-gray-200 rounded text-base hover:text-black text-white"
+                className="mr-3 rounded border-0 bg-[#488B80] px-6 py-2 text-base text-white hover:bg-gray-200 hover:text-black focus:outline-none lg:py-3"
               >
                 Sign up
               </button>
               <button
                 onClick={handleLogin}
-                className="border border-[#488B80] py-2 lg:py-3 px-6 mr-3 focus:outline-none hover:bg-gray-200 rounded text-base"
+                className="mr-3 rounded border border-[#488B80] px-6 py-2 text-base hover:bg-gray-200 focus:outline-none lg:py-3"
               >
                 Login
               </button>
             </div>
           )}
           {auth && (
-            <div className="hidden md:flex items-center">
+            <div className="hidden items-center md:flex">
               <button
-                className="bg-[#488B80] border-0 py-3 px-6 mr-3 focus:outline-none hover:bg-gray-200 rounded text-base hover:text-black text-white"
+                className="mr-3 rounded border-0 bg-[#488B80] px-6 py-3 text-base text-white hover:bg-gray-200 hover:text-black focus:outline-none"
                 onClick={() => setShowLogoutModal(true)}
               >
                 Logout
@@ -198,11 +200,11 @@ const Navbar = () => {
       </header>
 
       {showLogoutModal && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-500 bg-opacity-50 z-50 flex justify-center items-center">
-          <div className="bg-white p-4 rounded-md shadow-md">
-            <h3 className="text-lg font-medium mb-2">Confirm Logout</h3>
+        <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex items-center justify-center bg-gray-500 bg-opacity-50">
+          <div className="rounded-md bg-white p-4 shadow-md">
+            <h3 className="mb-2 text-lg font-medium">Confirm Logout</h3>
             <p className="text-gray-700">Are you sure you want to logout?</p>
-            <div className="flex justify-between mt-4">
+            <div className="mt-4 flex justify-between">
               <button className="btn btn-outline" onClick={handleCloseModal}>
                 Cancel
               </button>
@@ -214,6 +216,54 @@ const Navbar = () => {
         </div>
       )}
     </>
+  );
+};
+
+export const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      {/* Trigger */}
+      <button
+        className="flex items-center space-x-2 rounded-md bg-[#E5E7EB] py-1 text-lg text-black hover:bg-[#488B80] hover:text-white focus:outline-none px-4 lg:text-2xl"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span>Menu</span>
+        <svg
+          className={`h-4 w-4 transform transition-transform ${
+            isOpen ? "rotate-180" : ""
+          }`}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </button>
+
+      {/* Dropdown Menu */}
+      {isOpen && (
+        <div className="absolute mt-2 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          <div className="py-1 px-2">
+            {["Careers", "Blogs", "SGP"].map((item, idx) => (
+              <Link
+                to={"/" + item.toLowerCase()}
+                className="block py-2 text-lg text-black hover:text-[#488B80] lg:px-2"
+              >
+                {item}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
