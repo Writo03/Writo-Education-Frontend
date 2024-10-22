05f0f5bd4fa238ea174626827cb4bc449bf0ba1e
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-10-22 19:34:48 +0530

    update

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Navbar from '../Components/Layout/Navbar.jsx'
-import Footer from '../Components/Layout/Footer.jsx'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Navbar from "../Components/Layout/Navbar.jsx";
+import Footer from "../Components/Layout/Footer.jsx";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
-    <Navbar />
-    <Outlet />
-    <Footer />
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
