704edc720a21735e2d7686dd33d7113cb996d468
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-10-22 21:35:15 +0530

    /home test component mobile fix and improvement

--- client/src/Components/Test.jsx
+++ client/src/Components/Test.jsx
@@ -6,6 +6,32 @@ import { Autoplay, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 
+const TestData = [
+  {
+    id: 1,
+    title: "All India test",
+    subTitle: "Boost your Exam preparation",
+    description:
+      "Enhance your exam preparation with the All India Test Series. Achieve your academic goals and secure top ranks!",
+    button: "Join Today",
+  },
+  {
+    id: 2,
+    title: "Writo Mentorship",
+    subTitle: "Chart your course to success with personalized mentorsh",
+    description: "Fuel your growth with the power of mentorship|",
+    button: "Connect Now",
+  },
+  {
+    id: 3,
+    title: "Writo Student Community",
+    subTitle: "Connect with like-minded individuals",
+    description:
+      "Connect with individuals who are as excited about 6-10 Class Exam as you are, fostering an environment of collaboration and mutual growth.",
+    button: "Connect and collabrate",
+  },
+];
+
 const Test = () => {
   return (
     <div className="w-full">
@@ -39,70 +65,28 @@ const Test = () => {
           modules={[Autoplay, Navigation]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <div className="relative z-10 mx-auto flex h-full min-w-full flex-shrink-0 items-center justify-center">
-              <div className="flex w-full flex-col items-center lg:w-1/2">
-                <h1 className="mb-1 text-3xl font-bold text-[#85E8C5] md:mb-2 lg:mb-4">
-                  All India test
-                </h1>
-                <h2 className="mb-1 text-2xl text-[#85E8C5] md:mb-2 lg:mb-4">
-                  Boost your Exam preparation
-                </h2>
-                <p className="mb-1 px-8 text-center text-white md:px-24 lg:mb-2">
-                  Enhance your exam preparation with the All India Test Series.
-                  Achieve your academic goals and secure top ranks!
-                </p>
-                <a href="/jee-test-series" className="mt-4">
-                  <button className="rounded bg-[#488B80] px-4 py-2 font-semibold text-white hover:bg-[#e5e7eb] hover:text-black">
-                    Join Today
-                  </button>
-                </a>
+          {TestData.map((item, idx) => (
+            <SwiperSlide>
+              <div className="relative z-10 mx-auto flex h-full w-full flex-shrink-0 items-center justify-center p-6 md:p-2">
+                <div className="flex w-full flex-col items-center lg:w-1/2">
+                  <h1 className="mb-1 text-center text-3xl font-bold text-[#85E8C5] md:mb-2 lg:mb-4">
+                    {item.title}
+                  </h1>
+                  <h2 className="mb-1 text-center text-2xl text-[#85E8C5] md:mb-2 lg:mb-4">
+                    {item.subTitle}
+                  </h2>
+                  <p className="mb-1 px-8 text-center text-white md:px-24 lg:mb-2">
+                    {item.description}
+                  </p>
+                  <a href="/jee-test-series" className="mt-4">
+                    <button className="rounded bg-[#488B80] px-4 py-2 font-semibold text-white hover:bg-[#e5e7eb] hover:text-black">
+                      {item.button}
+                    </button>
+                  </a>
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="relative z-10 mx-auto flex h-full min-w-full flex-shrink-0 items-center justify-center">
-              <div className="flex w-full flex-col items-center lg:w-1/2">
-                <h1 className="mb-1 text-3xl font-bold text-[#85E8C5] md:mb-2 lg:mb-4">
-                  Writo Mentorship
-                </h1>
-                <h2 className="mb-1 text-2xl text-[#85E8C5] md:mb-2 lg:mb-4">
-                  Chart your course to success with personalized mentorsh
-                </h2>
-                <p className="mb-1 px-8 text-center text-white md:px-24 lg:mb-2">
-                  Fuel your growth with the power of mentorship|
-                </p>
-                <a href="/mentorship" className="mt-4">
-                  <button className="rounded bg-[#488B80] px-4 py-2 font-semibold text-white hover:bg-[#e5e7eb] hover:text-black">
-                    Connect Now
-                  </button>
-                </a>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="relative z-10 mx-auto flex h-full min-w-full flex-shrink-0 items-center justify-center">
-              <div className="flex w-full flex-col items-center lg:w-1/2">
-                <h1 className="mb-1 text-3xl font-bold text-[#85E8C5] md:mb-2 lg:mb-4">
-                  Writo Student Community
-                </h1>
-                <h2 className="mb-1 text-2xl text-[#85E8C5] md:mb-2 lg:mb-4">
-                  Like Minded Indiviuals shared interest,acadamic field,carrer
-                  goal
-                </h2>
-                <p className="mb-1 px-8 text-center text-white md:px-24 lg:mb-2">
-                  Connect with individuals who are as excited about 6-10 Class
-                  Exam as you are, fostering an environment of collaboration and
-                  mutual growth.
-                </p>
-                <a href="/community" className="mt-4">
-                  <button className="rounded bg-[#488B80] px-4 py-2 font-semibold text-white hover:bg-[#e5e7eb] hover:text-black">
-                    Connect and collabrate
-                  </button>
-                </a>
-              </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </section>
     </div>
