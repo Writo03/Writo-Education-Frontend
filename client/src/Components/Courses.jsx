59b064a9ebc4122d390fad69f22e60ea457e35b1
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-10-25 22:18:12 +0530

    /home aboutus fix

--- client/src/Components/Courses.jsx
+++ client/src/Components/Courses.jsx
@@ -5,11 +5,11 @@ const Courses = () => {
     <div className="my-4">
       <div className="flex items-center justify-center">
         <p className="item5 text-center text-2xl text-[#ED7E0A]">
-          - - - - About Us - - -
+          - - - -  Want to know more  - - -
         </p>
       </div>
       <div className="mt-5 flex items-center justify-center">
-        <p className="item1 text-center text-2xl text-[#488B80]">About Us</p>
+        <p className="item1 text-center text-2xl text-[#488B80]">About Us?</p>
       </div>
     </div>
   );
