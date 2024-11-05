25c42fd43ee51ef804dfd18aecdb6924ae85932e
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-11-05 22:19:39 +0530

    home improved

--- client/src/Components/ExpertGuidance_And_Doubt.jsx
+++ client/src/Components/ExpertGuidance_And_Doubt.jsx
@@ -37,7 +37,7 @@ const ExpertGuidance_And_Doubt = () => {
       {Data.map((item, idx) => (
         <div
           key={idx}
-          className="flex flex-col items-center justify-center bg-white px-4 py-4 mt-8"
+          className="mt-8 flex flex-col items-center justify-center bg-white px-4 py-4"
         >
           <h2 className="item5 mb-2 text-2xl text-[#ED7E0A]">
             - - - - {item.heading} -- - - -
@@ -45,18 +45,18 @@ const ExpertGuidance_And_Doubt = () => {
           <h1 className="coursetitle item2 mb-6 text-2xl text-green-700">
             {item.heading2}
           </h1>
-          <div className="flex w-full flex-col items-center lg:w-4/5 lg:flex-row">
+          <div className="group flex w-full flex-col items-center lg:w-4/5 lg:flex-row">
             {item.imgPos === "l" && !isMobile() && (
-              <div className="flex w-full justify-center lg:ml-10 lg:w-1/4">
+              <div className="flex h-[30vh] w-full justify-center overflow-hidden rounded-lg lg:ml-10 lg:w-1/4">
                 <img
                   src={item.image}
                   alt="Doubt clearing"
-                  className="rounded-lg shadow-md"
+                  className="h-full object-cover shadow-md transition-all duration-700 ease-in-out group-hover:scale-[1.03]"
                 />
               </div>
             )}
             {isMobile() && (
-              <div className="flex w-full justify-center lg:ml-10 lg:w-1/4">
+              <div className="flex w-full justify-center lg:w-1/4">
                 <img
                   src={item.image}
                   alt="Doubt clearing"
@@ -64,25 +64,27 @@ const ExpertGuidance_And_Doubt = () => {
                 />
               </div>
             )}
-            <div className="mb-6 flex w-full flex-col items-center lg:mb-0 lg:w-2/3">
-              <h2 className="mb-4 bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-center text-2xl font-bold">
-                {item.subHeading}
-              </h2>
-              <p className="item2 mb-4 text-center text-lg text-gray-800">
-                {item.description}
-              </p>
-              <Link to={item.buttonLink}>
-                <button className="item2 rounded-md bg-[#488B80] px-6 py-3 font-semibold text-white hover:bg-[#e5e7eb] hover:text-black focus:outline-none capitalize">
-                  {item.button}
-                </button>
-              </Link>
+            <div className="mb-6 flex w-full flex-col items-center lg:mx-10 lg:mb-0 lg:w-2/3">
+              <div className="flex flex-col items-start justify-center rounded-lg p-4 text-center">
+                <h2 className="mb-4 bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-center text-2xl font-bold">
+                  {item.subHeading}
+                </h2>
+                <p className="item2 mb-4 text-left text-lg text-gray-800">
+                  {item.description}
+                </p>
+                <Link to={item.buttonLink}>
+                  <button className="item2 rounded-md bg-[#488B80] px-6 py-3 font-semibold capitalize text-white hover:bg-[#e5e7eb] hover:text-black focus:outline-none">
+                    {item.button}
+                  </button>
+                </Link>
+              </div>
             </div>
             {item.imgPos === "r" && !isMobile() && (
-              <div className="flex w-full justify-center lg:ml-10 lg:w-1/4">
+              <div className="flex h-[30vh] w-full justify-center overflow-hidden rounded-lg lg:w-1/4">
                 <img
                   src={item.image}
                   alt="Doubt clearing"
-                  className="rounded-lg shadow-md"
+                  className="h-full object-cover shadow-md transition-all duration-700 ease-in-out group-hover:scale-[1.03]"
                 />
               </div>
             )}
